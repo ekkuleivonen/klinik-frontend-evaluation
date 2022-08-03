e6c86e915a6d2925151a20cde808f51185628e8b
Ekku Leivonen <92802215+ekkuleivonen@users.noreply.github.com>
2022-08-03 22:06:32 +0100

    typewriter effect

--- src/app/components/statistics-note/statistics-note.component.ts
+++ src/app/components/statistics-note/statistics-note.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { AnimalsService } from '../../services/animals.service';
 import type { Animal } from '../../services/animals.service';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-statistics-note',
@@ -37,7 +39,51 @@ export class StatisticsNoteComponent implements OnInit {
   constructor(service: AnimalsService) {
     service.getAllAnimals().then((data) => {
       this.animals = data;
+      this.launchTypeWriter();
     });
+  }
+  launchTypeWriter() {
+    const target = document.querySelector('#tw');
+    const options = {
+      loop: false,
+      typeSpeed: 70,
+    };
+    const writer1 = new Typewriter(target, options);
+    writer1
+      .type(
+        `Welcome to Klinik's animal shelter. -The space for our employees to share their common love towards our furry friends. `
+      )
+      .rest(500)
+      .type(
+        `Did you know that we have a whopping ${this.getDogCount()} dogs within our team? `
+      )
+      .rest(300)
+      .type(`Yup, we a real bunch of animal lovers over here! `)
+      .rest(300)
+      .type(`We do our best to provide our pets with the best homes possible. `)
+      .rest(300)
+      .type(
+        `In fact, we've done so well, that the average age of our animals is ${this.getAverageAgeOfAnimals()}. That's right, ${this.getAverageAgeOfAnimals()}! Do you think an animal would live up to that age in a poor home? `
+      )
+      .rest(300)
+      .type(
+        `We do our best to maintain a level playing field for our furry friends. `
+      )
+      .rest(300)
+      .type(`But as it seems, we do have our favourites. `)
+      .rest(300)
+      .type(
+        `${this.getMostCommonBreed()}s have taken the spotlight, and are now the most popular breed in our shelter. `
+      )
+      .rest(300)
+      .type(`Now, what does that say about us? `)
+      .rest(500)
+      .type(
+        `OK. That's enough. You came here to peek at some cute furry photos, didn't you? `
+      )
+      .rest(1000)
+      .type(`What are you waiting for? Click the button below!`)
+      .start();
   }
 
   ngOnInit(): void {}
